d98ec16284b0039ccb9abc342a71b9262fe128a9
Sofiya282 <sofiyasonu128@gmail.com>
2026-02-23 06:10:46 +0530

    Time: 548 ms (5.01%) | Memory: 47.4 MB (63.81%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -3,7 +3,16 @@ class Solution {
         for(int i = 0; i < nums.length; i++){
             nums[i] = nums[i]* nums[i];
         }
-        Arrays.sort(nums);
+        for(int i = 0; i<nums.length - 1;i++){
+            for (int j=0; j<nums.length -1 - i ;j++){
+                if (nums[j+1]<nums[j]){
+                    int temp = nums[j+1];
+                    nums[j+1]=nums[j];
+                    nums[j]=temp;
+                    
+                }
+            }
+        }
         return nums;
     }
 }
