ff280e483bbf8506aab0bbdd59bc494e6ad5387b
Sofiya282 <sofiyasonu128@gmail.com>
2026-02-25 21:06:55 +0530

    Time: 96 ms (5.14%) | Memory: 47.1 MB (13.70%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -1,9 +1,9 @@
-class Solution {
+class Solution {//1010
     public int countSetBits(int num){
         int count = 0;
         while(num > 0){
             count += (num & 1);
-            num >>= 1; 
+            num >>= 1;
         }
         return count;
     }
